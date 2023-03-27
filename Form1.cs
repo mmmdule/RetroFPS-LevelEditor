54f1c312f2c903b5a1fab6989fe76a8dfbfe7655
mmmdule <dulleyjedi@gmail.com>
2023-03-28 01:32:09 +0200

    ImagesAdded,Draw/Erase,Select/DrawMode,resizeCells

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LevelEditor.Properties;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,23 +9,49 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.Design;
 
 namespace LevelEditor
 {
     public partial class Form1 : Form
     {
+        //Image[] editorImages
+        List<Image> editorImages = new List<Image>{Resources.ArchwaySingle, Resources.ArchwaySmall,
+        Resources.ArmorBlink, Resources.Bullets, Resources.Imp, Resources.Cobweb_Wall,
+        Resources.DoorGate, Resources.EnergyBall, Resources.ExitDoor, Resources.Key,
+        Resources.ShotgunAmmo,Resources.SmallMedkit, Resources.Stone, Resources.Torch,
+        Resources.Tri_horn, Resources.wallBrick};
+
 
         public Form1()
         {
             this.DoubleBuffered = true;
             InitializeComponent();
+            SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.UserPaint | ControlStyles.OptimizedDoubleBuffer, true);
+            //typeof(TableLayoutPanel).GetProperty("DoubleBuffered", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance).SetValue(tableLayoutPanel1, true, null);
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+
             FillTablePanelOnLoad(tableLayoutPanel1);
             AddBasicLevelOutline(tableLayoutPanel1);
-            
+            AddPenChoiceButtonEvents(gameObjectsFlowLayoutPanel);
+        }
+        private bool resizingTable = false;
+        
+
+        private void AddPenChoiceButtonEvents(FlowLayoutPanel flowPanel)
+        {
+            for(int i = 0; i < flowPanel.Controls.Count; i++)
+            {
+                (flowPanel.Controls[i] as Button).Click += PenChoice_Click;
+            }
+        }
+
+        private void PenChoice_Click(object sender, EventArgs e)
+        {
+            currentPenImage = (sender as Button).BackgroundImage;
         }
 
         private void FillTablePanelOnLoad(TableLayoutPanel tablePanel)
@@ -40,12 +67,14 @@ namespace LevelEditor
             for (int i = 0; i < pictureBoxArr.Length; i++)
             {
                 pictureBoxArr[i] = new PictureBox();
+                pictureBoxArr[i].SizeMode = PictureBoxSizeMode.StretchImage;
                 //pictureBoxArr[i].Size = new Size(30, 30);
-                pictureBoxArr[i].BackColor = Color.FromArgb(128, 128, 0);
+                pictureBoxArr[i].BackColor = Color.Black;//Color.FromArgb(128, 128, 0);
                 pictureBoxArr[i].Margin = new Padding(0);
                 pictureBoxArr[i].Padding = new Padding(0);
 
-                pictureBoxArr[i].MouseMove += TablePictureBox_MouseMove;
+                pictureBoxArr[i].MouseMove += DrawOnMap;//TablePictureBox_MouseMove;
+                pictureBoxArr[i].MouseDown += DrawOnMap;
 
                 tablePanel.Controls.Add(pictureBoxArr[i]);
             }
@@ -55,41 +84,94 @@ namespace LevelEditor
             this.UseWaitCursor = false; //this == Form
         }
 
+
+        
+
         private void AddBasicLevelOutline(TableLayoutPanel tablePanel)
         {
             tablePanel.Enabled = false;
             tablePanel.SuspendLayout();
             for (int i = 0; i < tablePanel.RowCount; i++) {
                 if (i != 0 && i != tablePanel.RowCount - 1){
-                    (tablePanel.GetControlFromPosition(0, i) as PictureBox).BackColor = Color.Blue;
-                    (tablePanel.GetControlFromPosition(0, i) as PictureBox).MouseMove -= TablePictureBox_MouseMove;
-                    (tablePanel.GetControlFromPosition(tablePanel.ColumnCount - 1, i) as PictureBox).BackColor = Color.Blue;
-                    (tablePanel.GetControlFromPosition(tablePanel.ColumnCount - 1, i) as PictureBox).MouseMove -= TablePictureBox_MouseMove;
+                    (tablePanel.GetControlFromPosition(0, i) as PictureBox).Image = Resources.wallBrick;
+                    (tablePanel.GetControlFromPosition(0, i) as PictureBox).MouseMove -= DrawOnMap;
+                    (tablePanel.GetControlFromPosition(0, i) as PictureBox).MouseDown -= DrawOnMap;
+                    (tablePanel.GetControlFromPosition(tablePanel.ColumnCount - 1, i) as PictureBox).Image = Resources.wallBrick;
+                    (tablePanel.GetControlFromPosition(tablePanel.ColumnCount - 1, i) as PictureBox).MouseMove -= DrawOnMap;
+                    (tablePanel.GetControlFromPosition(tablePanel.ColumnCount - 1, i) as PictureBox).MouseDown -= DrawOnMap;
+
                     continue;
                 }
                 
                 for (int j = 0; j < tablePanel.ColumnCount; j++) {
-                    (tablePanel.GetControlFromPosition(j, i) as PictureBox).BackColor = Color.Blue;
-                    (tablePanel.GetControlFromPosition(j, i) as PictureBox).MouseMove -= TablePictureBox_MouseMove;
+                    (tablePanel.GetControlFromPosition(j, i) as PictureBox).Image = Resources.wallBrick;
+                    (tablePanel.GetControlFromPosition(j, i) as PictureBox).MouseMove -= DrawOnMap;
+                    (tablePanel.GetControlFromPosition(j, i) as PictureBox).MouseDown -= DrawOnMap;
                 }
             }
             tablePanel.ResumeLayout(true); 
             tablePanel.Enabled = true;
         }
 
-        private void TablePictureBox_MouseMove(object sender, MouseEventArgs e)
+        private Image currentPenImage = Resources.wallBrick; //pocetno stanje
+
+        private bool drawMode = true;
+        private void DrawOnMap(object sender, MouseEventArgs e)
         {
+            if (!drawMode)
+                return;
+
             PictureBox pictureBox = sender as PictureBox;
 
-            if (e.Button != MouseButtons.None)
+            if (e.Button == MouseButtons.Left)
             {
                 //MouseMove radi "Capture" nad misem ako se klikne taster.
                 //Kako bi se bojilo po drugim poljima, mora da se uradi "un-Capture"
                 if (pictureBox.Capture)
                     pictureBox.Capture = false;
-                
-                pictureBox.BackColor = Color.Black;
+
+                pictureBox.Image = currentPenImage;
             }
+            else if (e.Button == MouseButtons.Right)
+            {
+                if (pictureBox.Capture)
+                    pictureBox.Capture = false;
+
+                pictureBox.Image = null;
+            }
+            
+        }
+
+        private void resizeCellsButton_Click(object sender, EventArgs e)
+        {
+            ResizeCells(tableLayoutPanel1);
+        }
+
+        private void ResizeCells(TableLayoutPanel tableLayout)
+        {
+            float size = (float)numericUpDownCellSize.Value;
+
+            tableLayout.Visible = false;
+            tableLayout.Enabled = false;
+            tableLayout.SuspendLayout();
+            tableLayout.AutoScroll = false;
+            for (int i = 0; i < tableLayout.RowCount; i++)
+            {
+                tableLayout.RowStyles[i].Height = size;
+                tableLayout.ColumnStyles[i].Width = size;
+            }
+            tableLayout.ResumeLayout(true);
+            tableLayout.Invalidate(true);
+            tableLayout.Update();
+            tableLayout.Visible = true;
+            tableLayout.Enabled = true;
+            tableLayout.AutoScroll = true;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            gameObjectsFlowLayoutPanel.Enabled = radioButtonDrawMode.Checked;
+            drawMode = radioButtonDrawMode.Checked;
         }
     }
 }
--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace LevelEditor.Properties
-{
-
-
+namespace LevelEditor.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -19,52 +19,224 @@ namespace LevelEditor.Properties
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources
-    {
-
+    internal class Resources {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources()
-        {
+        internal Resources() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if ((resourceMan == null))
-                {
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("LevelEditor.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap ArchwaySingle {
+            get {
+                object obj = ResourceManager.GetObject("ArchwaySingle", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap ArchwaySmall {
+            get {
+                object obj = ResourceManager.GetObject("ArchwaySmall", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap ArmorBlink {
+            get {
+                object obj = ResourceManager.GetObject("ArmorBlink", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Bullets {
+            get {
+                object obj = ResourceManager.GetObject("Bullets", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Cobweb_Wall {
+            get {
+                object obj = ResourceManager.GetObject("Cobweb_Wall", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap cursor32 {
+            get {
+                object obj = ResourceManager.GetObject("cursor32", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap DoorGate {
+            get {
+                object obj = ResourceManager.GetObject("DoorGate", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap draw {
+            get {
+                object obj = ResourceManager.GetObject("draw", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap EnergyBall {
+            get {
+                object obj = ResourceManager.GetObject("EnergyBall", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap ExitDoor {
+            get {
+                object obj = ResourceManager.GetObject("ExitDoor", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Imp {
+            get {
+                object obj = ResourceManager.GetObject("Imp", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Key {
+            get {
+                object obj = ResourceManager.GetObject("Key", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap ShotgunAmmo {
+            get {
+                object obj = ResourceManager.GetObject("ShotgunAmmo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap SmallMedkit {
+            get {
+                object obj = ResourceManager.GetObject("SmallMedkit", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Stone {
+            get {
+                object obj = ResourceManager.GetObject("Stone", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Torch {
+            get {
+                object obj = ResourceManager.GetObject("Torch", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Tri_horn {
+            get {
+                object obj = ResourceManager.GetObject("Tri_horn", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap wallBrick {
+            get {
+                object obj = ResourceManager.GetObject("wallBrick", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
     }
